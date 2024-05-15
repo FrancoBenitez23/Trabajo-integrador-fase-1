3cf7dd57f23b4a1084d50a6db7df83ee7beacaaf
FrancoBenitez2 <32378799+FrancoBenitez23@users.noreply.github.com>
2024-05-15 20:25:10 -0300

    Update Pelicula.java

--- Integrador Fase 1/src/main/java/model/Pelicula.java
+++ Integrador Fase 1/src/main/java/model/Pelicula.java
@@ -1,25 +1,41 @@
 package model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Pelicula {
+
     private int codigo;
     private String titulo;
+    private String director;
     private String url;
-    private String generos;
-    private String imagen;
-
-    public Pelicula(String titulo, String url, String generos, String imagen) {
-        this.titulo = titulo;
-        this.url = url;
-        this.generos = generos;
-        this.imagen = imagen;
-    }
-
-    public Pelicula(int codigo, String titulo) {
-        this.codigo = codigo;
-        this.titulo = titulo;
-    }
+    private byte[] imagen;
+    private List<Genero> generos;
 
     public Pelicula() {
+        super();
+    }
+
+    public Pelicula(String titulo, String director, String url, byte[] imagen, List<Genero> generos) {
+        super();
+        this.titulo = titulo;
+        this.director = director;
+        this.url = url;
+        this.imagen = imagen;
+        this.generos = generos;
+    }
+
+    public Pelicula(int codigo, String titulo, String director, String url, byte[] imagen, List<Genero> generos) {
+        super();
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.director = director;
+        this.url = url;
+        this.imagen = imagen;
+        this.generos = generos;
+    }
+
+    public Pelicula(int codigo, String titulo, String director, String url, byte[] imagen) {
     }
 
     public int getCodigo() {
@@ -38,6 +54,14 @@ public class Pelicula {
         this.titulo = titulo;
     }
 
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
     public String getUrl() {
         return url;
     }
@@ -46,19 +70,31 @@ public class Pelicula {
         this.url = url;
     }
 
-    public String getGeneros() {
-        return generos;
-    }
-
-    public void setGeneros(String generos) {
-        this.generos = generos;
-    }
-
-    public String getImagen() {
+    public byte[] getImagen() {
         return imagen;
     }
 
-    public void setImagen(String imagen) {
+    public void setImagen(byte[] imagen) {
         this.imagen = imagen;
+    }
+
+    public List<Genero> getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(List<Genero> generos) {
+        this.generos = generos;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" +
+                "codigo=" + codigo +
+                ", titulo='" + titulo + '\'' +
+                ", director='" + director + '\'' +
+                ", url='" + url + '\'' +
+                ", imagen=" + Arrays.toString(imagen) +
+                ", generos=" + generos +
+                '}';
     }
 }
